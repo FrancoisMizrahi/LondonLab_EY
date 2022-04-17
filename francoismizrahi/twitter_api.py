8576829ba4e519da8254d6e48a2f6d8baea3cc25
Francois Mizrahi <francois.mizrahi@gmail.com>
2022-04-17 21:06:44 +0100

    Change code to use twitter v2 API

--- francoismizrahi/twitter_api.py
+++ francoismizrahi/twitter_api.py
@@ -1,18 +1,39 @@
-from authentification import API_Key, API_Key_Secret, Bearer_Token, Access_Token, Access_Token_Secret
-from tweepy import OAuthHandler, API, Cursor
+from authentification import Bearer_Token
+from tweepy import Client
 import pandas as pd
-import csv
-import re 
-import string
-import preprocessor as p
-  
-auth = OAuthHandler(API_Key, API_Key_Secret)
-auth.set_access_token(Access_Token, Access_Token_Secret)
- 
-api = API(auth,wait_on_rate_limit=True)
- 
-search_words = "Nasa"      # enter your words
-new_search = search_words + " -filter:retweets"
- 
-for tweet in Cursor(api.search_tweets,q=new_search,count=100,lang="en",since_id=0).items():
-    print(tweet.text)
+
+# create your client 
+client = Client(bearer_token=Bearer_Token)
+
+# query to search for tweets
+query = "#covid19 lang:en -is:retweet"
+# your start and end time for fetching tweets
+# start_time = "2021-12-10T00:00:00Z"
+# end_time = "2021-12-14T00:00:00Z"
+# get tweets from the API
+tweets = client.search_recent_tweets(query=query,
+                                     tweet_fields = ["created_at", "text", "source"],
+                                     user_fields = ["name", "username", "location", "verified", "description"],
+                                     max_results = 10,
+                                     expansions='author_id'
+                                     )
+
+# create a list of records
+tweet_info_ls = []
+# iterate over each tweet and corresponding user details
+for tweet, user in zip(tweets.data, tweets.includes['users']):
+    tweet_info = {
+        'created_at': tweet.created_at,
+        'text': tweet.text,
+        'source': tweet.source,
+        'name': user.name,
+        'username': user.username,
+        'location': user.location,
+        'verified': user.verified,
+        'description': user.description
+    }
+    tweet_info_ls.append(tweet_info)
+# create dataframe from the extracted records
+tweets_df = pd.DataFrame(tweet_info_ls)
+# display the dataframe
+print(tweets_df.head())
